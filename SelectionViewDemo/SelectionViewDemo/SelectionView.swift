04e86173585b0e6ff19df85363ca95951e778bf2
littlema <j620178@gmail.com>
2019-08-20 11:43:21 +0800

    optional protocol use AnyObject

--- SelectionViewDemo/SelectionViewDemo/SelectionView.swift
+++ SelectionViewDemo/SelectionViewDemo/SelectionView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-protocol SelectionViewDataSource: NSObject {
+protocol SelectionViewDataSource: AnyObject {
     
     func numberOfSelectionButtons(selectionView: SelectionView) -> Int
     
